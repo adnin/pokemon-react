78a68fec3f53d4c0975adcc4c4544b13344de4ec
Adnin Onofre <adninonofre@gmail.com>
2023-06-10 01:46:13 +0800

    add skeleton loader

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import ContentLoader from 'react-content-loader'
+
 import { AppDispatch, useAppDispatch } from '../redux';
 import { fetchPokemonbyId } from '../redux/pokemonReducer';
 import { PokemonDetails } from '../interfaces/Pokemon';
@@ -11,6 +13,19 @@ interface PokemonProps {
 }
 
 const SPLIT_STRING = 'https://pokeapi.co/api/v2/pokemon/'
+
+const Element = (props: any) => {
+    return (
+      <ContentLoader
+        backgroundColor="#ffe976"
+        foregroundColor="#fed87d"
+        speed={3}
+        {...props}
+      >
+        <rect x="0" y="0" rx="3" ry="3" width="400" height="560" />
+      </ContentLoader>
+    );
+  };
 
 export default function PokemonCard({name, url} : PokemonProps) {
     const [pokemonDetails, setPokemonDetails] = useState<PokemonDetails | null>(null)
@@ -27,23 +42,25 @@ export default function PokemonCard({name, url} : PokemonProps) {
         })
       }, [dispatch]);
 
-
   return (
     <div className='shadow bg-card-bg hover:bg-card-bg-hover hover:-translate-y-1 hover:scale-105 hover:border-2 duration-300 rounded border-4 border-card-border-color'>
-        {pokemonDetails && 
-            <img className="w-full" 
-                src={pokemonDetails.sprites.front_shiny} alt={`${name} avatar`} />
+        {pokemonDetails ? (
+            <>
+            <img className="w-full" src={pokemonDetails.sprites.front_shiny} alt={`${name} avatar`} />
+            <h1 className='text-center font-bold text-card-font-color text-xl capitalize mb-4'>{name}</h1>
+            <div className='card-container bg-card-name-bg p-2'>
+            <div className="grid grid-cols-3 gap-2">
+                {pokemonDetails.types.map((pokemonTypes: PokemonTypes, i) => (
+                    <PokemonBadge key={i} slot={pokemonTypes.slot} type={pokemonTypes.type} />
+                ))}
+            </div>
+            </div>
+            </>
+            ) : (
+            <div style={{ width: "100%", border: "1px solid" }}>
+                <Element viewBox="0 0 400 560" />
+            </div>)
         }
-        <h1 className='text-center font-bold text-card-font-color text-xl capitalize mb-4'>{name}</h1>
-        <div className='card-container bg-card-name-bg p-2'>
-            {pokemonDetails && 
-                <div className="grid grid-cols-3 gap-2">
-                    {pokemonDetails.types.map((pokemonTypes: PokemonTypes, i) => (
-                        <PokemonBadge key={i} slot={pokemonTypes.slot} type={pokemonTypes.type} />
-                    ))}
-                </div>
-            }
-        </div>
 
     </div>
   )
